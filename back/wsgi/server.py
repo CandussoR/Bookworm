aecb3e24d124eb238999422b20f952ad08a5e58f
Romain Candusso <r.candusso@protonmail.com>
2024-10-08 16:07:48 +0200

    feat : handle OPTIONS request for preflight, adding CORS, keep_alive connexion and content-length headers

--- back/wsgi/server.py
+++ back/wsgi/server.py
@@ -15,10 +15,14 @@ class MyHTTPService():
     @staticmethod
     def parse_request(request : bytes) -> dict:
         splitted_request = request.decode().split(NEW_LINE)
-        [method, uri, _] = splitted_request[0].split(" ")
         # Always creates at least an empty list for data, avoids unpacking errors
+        [method, uri, _] = splitted_request[0].split(" ")
         endpoint = ""
-        if method in ["GET", "DELETE"] :
+
+        if method == 'OPTIONS':
+            return {"method" : "OPTIONS"}
+
+        elif method in ["GET", "DELETE"] :
             if '?' in uri:
                 # With query params
                 endpoint, params = uri.rsplit("?", 1)
@@ -42,27 +46,33 @@ class MyHTTPService():
 
         if not data:
             data = None
-        else :
+        elif data and '+' in data:
+            # Cleans query params
             data = [d.replace('+', ' ') for d in data]
-            
         return {"method" : method, "uri" : endpoint if endpoint else uri, "data" : data}
-    
 
     @staticmethod
     def create_response_from(code, content_type, message):
-        status = {200 : "OK",
-                  201 : "Resource created",
-                  204 : "No content",
-                  400 : "Bad Request",
-                  404 : "Not found",
-                  500 : "Internal Server Error",
-                  501 : "Not Implemented"}
-        response = []
-        response.append(f'HTTP/1.1 {code} {status[code]}{NEW_LINE}')
-        response.append(f'Content-Type : {content_type}; charset=utf-8{NEW_LINE}')
-        response.append(f'Access-Control-Allow-Origin: http://localhost:5173{NEW_LINE}')
-        response.append(f'{NEW_LINE}')
-        response.append(message)
+        status = {
+            200: "OK",
+            201: "Resource created",
+            204: "No content",
+            400: "Bad Request",
+            404: "Not found",
+            500: "Internal Server Error",
+            501: "Not Implemented",
+        }
+        response = [
+            f"HTTP/1.1 {code} {status[code]}{NEW_LINE}",
+            f"Content-Type : {content_type}; charset=utf-8{NEW_LINE}",
+            f"Content-Length: {len(message.encode())}{NEW_LINE}",
+            f"Connection: keep-alive{NEW_LINE}",
+            f"Access-Control-Allow-Origin: http://localhost:5173{NEW_LINE}",
+            f"Access-Control-Allow-Methods: POST, PUT, DELETE, GET, OPTIONS{NEW_LINE}",
+            f"Access-Control-Allow-Headers: content-type{NEW_LINE}",
+            f"{NEW_LINE}",
+            message,
+        ]
         return "".join(response)
 
 
@@ -73,7 +83,7 @@ class RouterService():
 
         if not method in ['GET', 'POST', 'PUT', 'DELETE']:
             raise Exception("How the hell did you miss that?")
-        
+
         match uri:
             case '/api/authors':
                 return AuthorController(db, method, data)
@@ -83,22 +93,34 @@ class RouterService():
                 return CountryController(db, method, data)
             case '/api/readings':
                 return ReadingsController(db, method, data)
-            case 'api/reading_lists':
+            case '/api/reading_lists':
                 return ReadingListController(db, method, data)
-            # case '/api/search':
             case _:
                 raise Exception("err... what did you want again ?")
 
 
-class MyTCPHandler(socketserver.BaseRequestHandler): 
+class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         request = self.request.recv(1024).strip()
-        print(request)
+        print(request, type(request))
         decoded_request = MyHTTPService.parse_request(request)
-        print(decoded_request)
-        env = json.loads(open('back/env.json', 'r').read())
-        with sqlite_db.connect(env["database_test"]) as conn:
-            code, message = RouterService.call_service(decoded_request["uri"], decoded_request["method"], conn, decoded_request["data"]).do()
-            http_response = MyHTTPService.create_response_from(code, "application/json", str(message))
-            encoded_response = http_response.encode()
-            self.request.sendall(encoded_response)
+        if decoded_request["method"] == 'OPTIONS':
+            code, message = 200, ""
+        else:
+            print(decoded_request)
+            env = json.loads(open('back/env.json', 'r').read())
+            with sqlite_db.connect(env["database_test"]) as conn:
+                service = RouterService.call_service(
+                    decoded_request["uri"],
+                    decoded_request["method"],
+                    conn,
+                    decoded_request["data"],
+                )
+                # Ignoring type because all the methods have to be implemented
+                # in child classes or app should break.
+                code, message = service.do() #type: ignore
+                print("code", code, "\nmessage", message)
+        http_response = MyHTTPService.create_response_from(code, "application/json", str(message))
+        encoded_response = http_response.encode()
+        print("encoded response\n", encoded_response)
+        self.request.sendall(encoded_response)
